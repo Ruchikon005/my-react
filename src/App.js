06c536fd53120af7bec84b8725335f1667a936a2
Ruchikon005 <6135512005@psu.ac.th>
2020-09-16 04:33:29 -0700

    Update5-addlogo+Customize

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import './App.css';
 import WordCard from './WordCard';
-
+import logo from './Logo.png';
+import logo1 from './logo.svg';
 
 function App() {
     return (
-      <div className='center' >
-        <h1 className='backTitle'>GAME</h1>
+      <div className = 'center'>
+        
+        <h1 className='Title'><p className = 'center'>REACT<img src={logo1} className="App-logo2" alt="logo1" />GAME</p></h1>
         <a href ='#'><WordCard value="hello" /></a>
+        <h3>Click to arrange the words correctly.</h3>
+        <img src={logo} className="App-logo" alt="logo" />
       </div>
     );
   }
